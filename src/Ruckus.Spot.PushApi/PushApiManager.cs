c276148f82a24997c3a0f127ecfdb4ce8816674d
Andrey Kurdyumov <kant2002@gmail.com>
2016-03-29 17:30:42 +0600

    Add disconnect option to the server

--- src/Ruckus.Spot.PushApi/PushApiManager.cs
+++ src/Ruckus.Spot.PushApi/PushApiManager.cs
@@ -14,12 +14,19 @@ namespace Ruckus.Spot.PushApi
     /// </summary>
     public class PushApiManager
     {
+        private readonly string[] Topics = new string[] { "1.0.0/LOC/SPOT_GPB/#" };
         private string server;
         private string login;
         private string password;
 
         private MqttClient client;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PushApiManager"/> class.
+        /// </summary>
+        /// <param name="server">Host name or address of the Ruskus Push API server</param>
+        /// <param name="login">Login on the server.</param>
+        /// <param name="password">Password to the server.</param>
         public PushApiManager(string server, string login, string password)
         {
             this.server = server;
@@ -28,12 +35,32 @@ namespace Ruckus.Spot.PushApi
             this.client = new MqttClient(server);
         }
 
+        /// <summary>
+        /// Called when new coordinates calculated by Ruckus Push API
+        /// </summary>
         public event Action<LocationMessage> OnLocationReceived;
 
+        /// <summary>
+        /// Called when connection to Ruckus Push API terminated.
+        /// </summary>
         public event EventHandler ConnectionClosed;
 
+        /// <summary>
+        /// Called when connection to Ruckus Push API finished.
+        /// </summary>
         public event EventHandler<MqttMsgSubscribedEventArgs> Subscribed;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether connection to Ruckus Push API established.
+        /// </summary>
+        public bool IsConnected
+        {
+            get { return this.client.IsConnected; }
+        }
  
+        /// <summary>
+        /// Connect to the Ruckus Push API server.
+        /// </summary>
         public void Connect()
         {
             string clientId = Guid.NewGuid().ToString();
@@ -47,25 +74,30 @@ namespace Ruckus.Spot.PushApi
                 throw new InvalidOperationException("Could not connect");
             }
 
-            this.client.Subscribe(new string[] { "1.0.0/LOC/SPOT_GPB/#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            this.client.Subscribe(Topics, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        /// <summary>
+        /// Disconnects from the Ruckus Push API server.
+        /// </summary>
+        private void Disconnect()
+        {
+            if (!this.client.IsConnected)
+            {
+                throw new InvalidOperationException();
+            }
+
+            this.client.Unsubscribe(Topics);
         }
 
         private void Client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
         {
-            var handler = this.Subscribed;
-            if (handler != null)
-            {
-                handler(sender, e);
-            }
+            this.Subscribed?.Invoke(sender, e);
         }
 
         private void Client_ConnectionClosed(object sender, EventArgs e)
         {
-            var handler = this.ConnectionClosed;
-            if (handler != null)
-            {
-                handler(sender, e);
-            }
+            this.ConnectionClosed?.Invoke(sender, e);
         }
 
         private void OnMqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
@@ -79,6 +111,11 @@ namespace Ruckus.Spot.PushApi
         private void UpdateMessages(IEnumerable<LocationMessage> messages)
         {
             var handler = this.OnLocationReceived;
+            if (handler == null)
+            {
+                return;
+            }
+
             foreach (var msg in messages)
             {
                 handler(msg);
